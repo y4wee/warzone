e13d2b037ba4d15949aa6a140605093e3d30f285
y4wee <quentin.jambert@gmail.com>
2022-07-25 20:43:57 +0200

    matches css

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -21,28 +21,28 @@ const Matches = (props) => {
         <div className="matches">
             {props.data.map((matche, index) => (
                 <div className="matchesCard" key={index}>
-                    <div className="matchesMode">
+                    <div className="matchesMode matchesCardDetail">
                         {changeName(matche[1].mode)}
                     </div>
-                    <div className="matchesPlacement">
+                    <div className="matchesPlacement matchesCardDetail">
                         {"Top " + matche[1].playerStats.teamPlacement}
                     </div>
-                    <div className="matchesKill">
+                    <div className="matchesKill matchesCardDetail">
                         {"Kills : " + matche[1].playerStats.kills}
                     </div>
-                    <div className="matchesDeath">
+                    <div className="matchesDeath matchesCardDetail">
                         {"Deaths : " + matche[1].playerStats.deaths}
                     </div>
-                    <div className="matchesRatio">
+                    <div className="matchesRatio matchesCardDetail">
                         {"Ratio : " + matche[1].playerStats.kdRatio.toFixed(2)}
                     </div>
-                    <div className="matchesScore">
+                    <div className="matchesScore matchesCardDetail">
                         {"Score : " + matche[1].playerStats.score}
                     </div>
-                    <div className="matchesDmgDone">
+                    <div className="matchesDmgDone matchesCardDetail">
                         {"Damages done : " + matche[1].playerStats.damageDone}
                     </div>
-                    <div className="matchesDmgTaken">
+                    <div className="matchesDmgTaken matchesCardDetail">
                         {"Damages taken : " + matche[1].playerStats.damageTaken}
                     </div>
                 </div>
